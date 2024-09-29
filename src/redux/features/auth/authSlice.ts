238a224fe75fa7dd37e4394a3ab0a4a477afb316
ShamimaNasrin <shamima.nasrin007@gmail.com>
2024-09-29 22:08:29 +0600

    redux auth

---
+++ src/redux/features/auth/authSlice.ts
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../store";
+
+export type TUser = {
+  _id: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  role: string;
+};
+
+type TAuthState = {
+  user: null | TUser;
+  token: null | string;
+};
+
+const initialState: TAuthState = {
+  user: null,
+  token: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, actions) => {
+      const { user, token } = actions.payload;
+      state.user = user;
+      state.token = token;
+    },
+    logOut: (state) => {
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+export const { setUser, logOut } = authSlice.actions;
+
+export default authSlice.reducer;
+
+export const useCurrentToken = (state: RootState) => state.auth.token;
+export const useCurrentUser = (state: RootState) => state.auth.user;
